2e0e2f619f96e36811c2f23aaa12f6c6aa048c02
ion-golovco <gaminglions2@gmail.com>
2020-09-07 21:45:22 +0300

    Update mobile cards descriptions
    Different card description for mobile

--- src/components/FavoriteProjects.jsx
+++ src/components/FavoriteProjects.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
 import { Card, CardBody, CardText, Container, CardImg, Row, Col, CardLink } from "reactstrap";
+import { isMobile } from "react-device-detect";
 
 const items = [
     {
         desc: "Lost fear - a short side scroller made in JS Canvas Coming Soon",
         link: "",
         image: "https://i.imgur.com/CzRzOib.png",
+        mobiledesc: "Lost fear - a short side scroller",
     },
     {
         desc: "The shadow of the chaos game, provably discovered by me :) ",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/index/chaos/",
         image: "https://i.imgur.com/8Bc4M9H.png",
+        mobiledesc: "Chaos game",
     },
     {
         desc: "A solar system with hitboxes for the moons and planets",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/p5articles/p5solarsystem",
         image: "https://i.imgur.com/yRRFDob.png",
+        mobiledesc: "Solar System",
     },
     {
         desc: "Fractals are really cool",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/p5articles/p5jsfractals",
         image: "https://i.imgur.com/vfNBJiS.png",
+        mobiledesc: "Fractals are really cool",
     },
 ];
 
@@ -36,9 +41,11 @@ class FavoriteProjects extends Component {
     createSlide = (a) => {
         let link =
             a.link !== "" ? (
-                <CardLink id ="cardlink" href={a.link}>{a.desc}</CardLink>
+                <CardLink id="cardlink" href={a.link}>
+                    {isMobile ? a.mobiledesc : a.desc}
+                </CardLink>
             ) : (
-                <CardText>{a.desc}</CardText>
+                <CardText>{isMobile ? a.mobiledesc : a.desc}</CardText>
             );
 
         return (
