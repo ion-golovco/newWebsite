6d60ff5c9e346b9cdd21256f296fd3e4ff9dcd6b
ion-golovco <gaminglions2@gmail.com>
2020-10-19 15:11:49 +0300

    mostly done

--- src/assets/items.jsx
+++ src/assets/items.jsx
@@ -11,13 +11,13 @@ const items = [
         desc: "Lost fear - a short side scroller made in JS Canvas Coming Soon Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-        id:5,
+        id:2,
         title:"My new logo",
         date:"",
         desc: (<Container>Fractals are really cool arent they</Container>),
     },
     {
-        id: 2,
+        id: 3,
         title: "Chaos game shadows",
         image: "https://i.imgur.com/8Bc4M9H.png",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/index/chaos/",
@@ -25,7 +25,7 @@ const items = [
         desc: "The shadow of the chaos game, provably discovered by me :) ",
     },
     {
-        id: 3,
+        id: 4,
         title: "Solar System, v1?",
         image: "https://i.imgur.com/yRRFDob.png",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/p5articles/p5solarsystem",
@@ -34,7 +34,7 @@ const items = [
         
     },
     {
-        id: 4,
+        id: 5,
         title: "L-System fractals",
         image: "https://i.imgur.com/vfNBJiS.png",
         link: "https://hereyougo.netlify.app/p5jslibraryandcode/p5articles/p5jsfractals",
